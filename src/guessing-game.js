f9a189122aa7d4444e4b2d0de1c4894f95baf969
arianspacerock <siropemptyspace@gmail.com>
2021-09-01 11:57:10 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.res = Math.round((this.min + this.max) / 2);
+      return this.res;
     }
-
+  
     lower() {
-
+      this.max = this.res;
     }
-
+  
     greater() {
-
+      this.min = this.res;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
